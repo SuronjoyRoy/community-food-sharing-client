697c328e0c73882ac126944d140855d5f345518a
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-11-09 12:34:03 +0600

    env enviroment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,23 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCtJnYrBh4iO2z9Q1skAvj5uNJTZdB_E0s",
-  authDomain: "community-food-sharing-85ff9.firebaseapp.com",
-  projectId: "community-food-sharing-85ff9",
-  storageBucket: "community-food-sharing-85ff9.appspot.com",
-  messagingSenderId: "900078307666",
-  appId: "1:900078307666:web:38802765ee1fdd8b93d28b"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDERID,
+  appId: import.meta.env.VITE_APPID
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCtJnYrBh4iO2z9Q1skAvj5uNJTZdB_E0s",
+//   authDomain: "community-food-sharing-85ff9.firebaseapp.com",
+//   projectId: "community-food-sharing-85ff9",
+//   storageBucket: "community-food-sharing-85ff9.appspot.com",
+//   messagingSenderId: "900078307666",
+//   appId: "1:900078307666:web:38802765ee1fdd8b93d28b"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
